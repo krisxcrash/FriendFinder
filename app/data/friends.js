9820f0b5aa0a0fd93e18a2e7f4246f81c759fa4c
Kristine Martin <kristinemartin@kristines-mbp.attlocal.net>
2017-09-12 23:01:45 -0700

    updated friends.js, apiRoutes.js, and server.js

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+// Friendly matches
+
+var newFriends = [{
+    "name": "Frank",
+    "photo": "http://rivista-cdn.reptilesmagazine.com/images/cache/cache_6/cache_e/cache_a/bearded-dragon-care-5001-4d13fae6.jpeg?ver=1503612446&aspectratio=1.5923566878981",
+    "scores": [
+        5,
+        1,
+        2,
+        5,
+        3,
+        4,
+        2,
+        5,
+        5,
+        2
+    ]
+}, {
+    "name": "Sadie",
+    "photo": "http://cdn.akc.org/akccontentimages/BreedOfficialPortraits/hero/Australian-Shepherd.jpg",
+    "scores": [
+        3,
+        4,
+        3,
+        1,
+        2,
+        4,
+        4,
+        3,
+        5,
+        1
+    ]
+}, {
+    "name": "Bubba",
+    "photo": "http://olnl.net/u23/photoB4E3/20589332951-0/original.jpg",
+    "scores": [
+        4,
+        3,
+        3,
+        2,
+        5,
+        5,
+        1,
+        2,
+        3,
+        5
+    ]
+}, {
+    "name": "Kiwi",
+    "photo": "https://www.mpg.de/9327999/zoom-1437031803.jpg",
+    "scores": [
+        2,
+        1,
+        4,
+        5,
+        5,
+        2,
+        3,
+        4,
+        4,
+        3
+    ]
+}, {
+    "name": "Penelope",
+    "photo": "https://media4.s-nbcnews.com/j/newscms/2016_36/1685951/ss-160826-twip-05_8cf6d4cb83758449fd400c7c3d71aa1f.nbcnews-ux-2880-1000.jpg",
+    "scores": [
+        1,
+        1,
+        2,
+        4,
+        5,
+        4,
+        3,
+        5,
+        3,
+        2
+    ]
+}, {
+    "name": "Abby",
+    "photo": "https://i.pinimg.com/736x/dc/01/be/dc01be042adceea4d034f3bd640c831c--endangered-species-amphibians.jpg",
+    "scores": [
+        4,
+        2,
+        3,
+        1,
+        1,
+        5,
+        4,
+        2,
+        4,
+        1
+    ]
+}];
+
+//exports the array
+
+module.exports = newFriends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,21 @@
+var newFriends = require("../data/friends.js");
 
-app.get("/api/friends", function(req, res) {
-    var chosen = req.params.characters;
-  
-    if (chosen) {
-      console.log(chosen);
-  
-      for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-          return res.json(characters[i]);
-        }
-      }
-      return res.json(false);
-    }
-    return res.json(characters);
-  });
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json(newFriends);
+    });
+
+    app.post("/api/new", function(req, res) {
+        
+    })
+}
+
 
 // User's information from Survey - takes in JSON input
 app.post("/api/new", function(req, res) {
     var newfriend = req.body;
     newfriend.routeName = newfriend.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newfriend);
   
     characters.push(newfriend);
   
