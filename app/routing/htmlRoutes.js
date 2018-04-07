33d5a4e5d5d2b561f7cf803d45bfb274b401a1d9
Kristine Martin <kristinenicolemartin@gmail.com>
2018-04-07 11:58:45 -0700

    updates

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 	//HTML GET requests
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/home.html"));
-	})
+	});
 	app.get("/survey", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
